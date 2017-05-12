b7fa636d7d7a4371758231881b68b003835ce456
Joshua Bradford <jjyet@yahoo.com>
2017-05-11 23:17:07 -0400

    adding room for second day, need to create system, allow users to enter data

--- src/App.js
+++ src/App.js
@@ -10,16 +10,12 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src="https://rlv.zcache.com/inspiring_weight_loss_quote_magnet-re81c83f0b74843db90bf4a119aa1e12a_x7js9_8byvr_324.jpg" className="App-logo" alt="logo" />
           <h2>Welcome to our weight loss journey :)</h2>
         </div>
         <p className="App-intro">
           This app is for people who like to lose weight!
         </p>
-
-
-
-
 
         <div className="progress-photo-container">
           <img src={may11a} className="progress-photo" alt="" />
@@ -33,7 +29,17 @@ class App extends Component {
           <img src={may11c} className="progress-photo" alt="" />
         </div>
 
+        <div>
+          Today was May 11, 2017. I started two weeks ago at 228.6 lbs
+          I am down 9 pounds already! Amanda has been at it for 2 years and
+          she has lost 50 pounds she is down to 215 lbs!
+        </div>
+
+        <div className="date-container">May 12, 2017</div>
+        <div className="weight-container">unknown</div>
       </div>
+
+
     );
   }
 }
